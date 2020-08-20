e1f2815ef9cd0765d6ca48e25f25589aced063f6
qianxu2001 <62287903+qianxu2001@users.noreply.github.com>
2020-08-20 16:22:10 +0800

    Update CheatManagerOverlay.cpp

--- 3ds/source/CheatManagerOverlay.cpp
+++ 3ds/source/CheatManagerOverlay.cpp
@@ -59,8 +59,8 @@ CheatManagerOverlay::CheatManagerOverlay(Screen& screen, const std::string& mkey
 
     staticBuf  = C2D_TextBufNew(48);
     dynamicBuf = C2D_TextBufNew(16);
-    C2D_TextParse(&multiSelectText, staticBuf, "\uE003 to select all cheats");
-    C2D_TextParse(&multiDeselectText, staticBuf, "\uE003 to deselect all cheats");
+    C2D_TextParse(&multiSelectText, staticBuf, "\uE003 全选");
+    C2D_TextParse(&multiDeselectText, staticBuf, "\uE003 取消全选");
     C2D_TextOptimize(&multiSelectText);
     C2D_TextOptimize(&multiDeselectText);
 }
@@ -139,7 +139,7 @@ void CheatManagerOverlay::update(touchPosition* touch)
             g_selectedCheatCodes.push_back(scrollable->cellName(i));
         }
         me = std::make_shared<YesNoOverlay>(
-            screen, "Do you want to store\nthe cheat file?",
+            screen, "保存作弊文件?",
             []() {
                 CheatManager::getInstance().save(g_selectedCheatKey, g_selectedCheatCodes);
                 g_screen->removeOverlay();
